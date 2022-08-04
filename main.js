74062a1ee77f23380f91c49a6619a5c032be5804
Commando12369 <76631745+Commando12369@users.noreply.github.com>
2022-08-04 13:26:41 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 //required classes
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, GatewayIntentBits, EmbedBuilder} = require('discord.js');
+const { Client, Collection, GatewayIntentBits, Partials, EmbedBuilder } = require('discord.js');
 const { token } = require('./config.json');
+const { waitForDebugger } = require('node:inspector');
 
 //create client
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages]});
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.DirectMessages, GatewayIntentBits.MessageContent], partials: [Partials.Channel] });
 
 //creating commands collection and locating files in commands folder
 client.commands = new Collection();
@@ -17,6 +18,19 @@ for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
 	const command = require(filePath);
 	client.commands.set(command.data.name, command);
+}
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
 }
 
 //getting the command (whatever the user types) from client.commands Collection
@@ -36,23 +50,33 @@ client.on('interactionCreate', async interaction => {
 });
 
 //adding roles to new members
-client.on('guildMemberAdd', member =>{
+client.on('guildMemberAdd', member => {
 
-    const channel = member.guild.channels.cache.find(channel => channel.name === "welcome");
-    if(!channel) return;
+	const channel = member.guild.channels.cache.find(channel => channel.name === "welcome");
+	if (!channel) return;
 
-    channel.send(`Welcome to nshs, ${member}, please read the rules in the info channel!`)
+	channel.send(`Welcome to nshs.life, ${member}, please check your DMs for steps to join!`)
 	member.roles.add(member.guild.roles.cache.get('1004509586142806087'))
 
 	const Embed = new EmbedBuilder()
 		.setThumbnail(client.user.displayAvatarURL())
 		.setTitle('Welcome to nshs.life!')
 		.setColor(0x18e1ee)
-		.addFields({ name: 'Our Mission', value: '[mission.nshs.life](https://docs.google.com/document/u/5/d/e/2PACX-1vToUA9QApqWmo_k5YGaouh1-FexC5tqLzUIZv6fJZGneyBZwM_ImYNDzraq3mT5FzQVS_EGC7Kdk_Oj/pub)'})
-		.addFields({ name: 'Join Requirement', value: 'Please enter your school LASID'});
+		.addFields({ name: 'Our Mission', value: '[mission.nshs.life](https://docs.google.com/document/u/5/d/e/2PACX-1vToUA9QApqWmo_k5YGaouh1-FexC5tqLzUIZv6fJZGneyBZwM_ImYNDzraq3mT5FzQVS_EGC7Kdk_Oj/pub)' })
+		.addFields({ name: 'Join Requirement', value: 'Please enter your school LASID' });
 
-		member.send({ embeds: [Embed] })
+	member.send({ embeds: [Embed] })
 })
 
+//replying to DMs
+client.on('messageCreate', msg => {
+
+	if (msg.channel.type == 1 && msg.author != client.user) {
+		console.log(msg.content)
+		msg.channel.send("hello there")
+		
+	}
+
+});
 
 client.login(token);
